00f8f4a4d9cdfe675add9a194be5171ca9fdfac2
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-09-02 00:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
-    //Write your code here
-  
+    let table = document.getElementById('myTable');
+    table.innerHTML = '';
+
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    for (let i = 0; i < rn; i++) {
+        let row = document.createElement('tr');
+		
+        for (let j = 0; j < cn; j++) {
+            let cell = document.createElement('td');
+            cell.textContent = `Row-${i} Column-${j}`;
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
 }
